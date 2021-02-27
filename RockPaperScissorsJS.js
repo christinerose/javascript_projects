7b70614bffb6b2d77f41378f6326142a7986811e
Christine Rose <professor.rose@gmail.com>
2021-02-27 15:38:22 -0800

    IT'S WORKING NOW!!!

--- RockPaperScissorsJS.js
+++ RockPaperScissorsJS.js
@@ -1,89 +1,69 @@
 
 //Practicing JavaScript Syntax
 
+window.confirm("Let's play Rock, Paper, Scissors!");
+
+//const ROCK = 1;
+//const PAPER = 2;
+//const SCISSORS = 3;
+// Start with these constants, and then replace the 1, 2, 3 with the constant names
+// Doing it now resulted in lots of errors "ROCK has already been defined" 
+// because I'm testing it in a browser console over and over
+// FOR NOW! IT WORKS LIKE THIS! (even if it's not as readable as it could be)
 
 function Rock_Paper_Scissors() {
-    console.log(" ");
-    console.log("**********");
-    console.log(" ");
 
-    console.log("Let's play Rock, Paper, Scissors!");
-
-    i = Math.floor(Math.random() * 4);
+    i = Math.floor(Math.random() * 3) + 1;
     play = i;
 
     console.log(play);
 
-    console.log("1. Rock");
-    console.log("2. Paper");
-    console.log("3. Scissors");
+    let guess = prompt("Choose one of these numbers to play:\n1. Rock\n2. Paper\n3. Scissors\nSee if you can beat me!\nOne, two, three—GO! ");
 
-    console.log(" ");
-    console.log("**********");
-    console.log(" ");
-
-    let guess = prompt("Choose a number above to throw Rock, Paper, or Scissors. See if you can beat me! One, two, three—GO! ");
-
-    if (guess === play) {
+    if (parseInt(guess) === play) {
         let p = play;
         if (p === 1) {
-            console.log(" ");
-            console.log("I threw a Rock too! It's a TIE! ");
+            window.confirm("I threw a Rock too! It's a TIE!");
         } else if (p === 2) {
-            console.log(" ");
-            console.log("I threw Paper too! It's a TIE! ");
+            window.confirm("I threw Paper too! It's a TIE!?");
         } else {
-            console.log(" ");
-            console.log("I threw Scissors too! It's a TIE! ");
+            window.confirm("I threw Scissors too! It's a TIE! ");
         }
-    } else if (guess === 1 && play != 1) {
-        switch (play === 2) {
-            case paper:
-                console.log("I threw PAPER. Paper covers Rock. I win! ");
+    } else if (parseInt(guess) === 1 && play != 1) {
+        switch (play) {
+            case 1:
+                window.confirm("I threw PAPER. Paper covers Rock. I win!   ");
                 break;
-            case scissors: // or just default: ??
-                console.log("I threw SCISSORS. Rock breaks Scissors. You win! ");
+            case 3: // or just default: ??
+                window.confirm("I threw SCISSORS. Rock breaks Scissors. You win!   ");
                 break;
         }
-    } else if (guess === 2 && play != 2) {
-        switch (play === 1) {
-            case rock:
-                console.log("I threw a ROCK. Paper covers Rock. You win! ");
+    } else if (parseInt(guess) === 2 && play != 2) {
+        switch (play) {
+            case 1:
+                window.confirm("I threw a ROCK. Paper covers Rock. You win!   ");
                 break;
             default:
-                console.log("I threw SCISSORS. Scissors cuts Paper. I win! ");
+                window.confirm("I threw SCISSORS. Scissors cuts Paper. I win!   ");
         }
-    } else if (guess == 3 && play != 3) {
+    } else if (parseInt(guess) == 3 && play != 3) {
         if (play == 1) {
-            console.log(" ");
-            console.log("I threw a ROCK! Rock breaks Scissors. I win! ");
+            window.confirm("I threw a ROCK! Rock breaks Scissors. I win!   ");
         } else {
-            console.log(" ");
-            console.log("I threw PAPER. Scissors cuts Paper. You win! ");
+            window.confirm("I threw PAPER. Scissors cuts Paper. You win!   ");
         }
     } else {
-        console.log("Ooops! Something went wrong! ");
+        window.confirm("Ooops! Something went wrong! ");
     }
-
-    console.log(" ");
-    console.log("**********");
-    console.log(" ");
-
-
-
 
 
     let answer = prompt("What to play again? Y or N? ");
 
     if (answer === "Y" || answer === "y") {
-        console.log(" ");
-        console.log("Great! Here we go!");
-        console.log(" ");
+        window.confirm("Great! Here we go!");
         Rock_Paper_Scissors();
     } else {
-        console.log(" ");
-        console.log("Thank you for playing! See you next time!");
-        console.log(" ");
+        window.confirm("Thank you for playing! See you next time!");
     }
 }
 
